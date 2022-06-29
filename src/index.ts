b253d3e0fc134f59d365829aa5111e4028ae7c75
James King <me@jamesking.email>
2022-06-29 02:14:47 -0400

    Now even more annoying

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,8 @@ const audioFiles: Dictionary<string[]> = {
         'Noot',
         'Body Reported',
         'Emergency Meeting',
+        'smash',
+        'bark4me'
     ],
 
     'Music': [
@@ -27,9 +29,12 @@ const audioFiles: Dictionary<string[]> = {
         'I have so far to go',
         'Clubbed to death',
         'Monster Mashturbate',
+        'Crazy Bong',
     ],
 
     'Memes': [
+        'Thanks Obama',
+        'OBAMNA',
         'Among Us',
         'Nice',
         'Disgusting',
@@ -49,6 +54,7 @@ const audioFiles: Dictionary<string[]> = {
     ],
 
     'Quotes': [
+        'We Saved The City',
         'trumpgender',
         'George Bush doesnt care about black people',
         'Gay frogs',
@@ -134,12 +140,15 @@ const audioFiles: Dictionary<string[]> = {
 }
 
 const controlContainerRef = dom.ref<HTMLDivElement>('control-container')
-const audioSliderRef = dom.ref<HTMLInputElement>('volume-slider')
+const volumeSliderRef = dom.ref<HTMLInputElement>('volume-slider')
+const pitchSliderRef = dom.ref<HTMLInputElement>('pitch-slider')
 const playAllRef = dom.ref<HTMLButtonElement>('play-button')
 const stopAllRef = dom.ref<HTMLButtonElement>('stop-button')
 const playFriendsRef = dom.ref<HTMLButtonElement>('friends-button')
+const loopModeRef = dom.ref<HTMLButtonElement>('loop-button')
 
-let audioControls: AudioControl[] = []
+const audioControls: AudioControl[] = []
+let loopMode = false
 
 function getButtonPlayTimeBackground(playTimeAmount: number) {
     return `linear-gradient(to right, #4d8050 ${playTimeAmount * 100}%, #6cb370 ${playTimeAmount * 100}%)`
@@ -159,11 +168,34 @@ function animate() {
 
 requestAnimationFrame(animate)
 
+function getSliderValue(slider: HTMLInputElement) {
+    return Number.parseInt(slider.value)
+}
+
 async function updateVolume() {
+    const audioSlider = await volumeSliderRef.get()
+    const value = getSliderValue(audioSlider) / 100
     for (const { audioGain } of audioControls) {
-        const audioSlider = await audioSliderRef.get()
-        audioGain.gain.setValueAtTime(Number.parseInt(audioSlider.value) / 100, 0)
+        audioGain.gain.setValueAtTime(value, 0)
     }
+}
+
+async function updatePitch() {
+    const pitchSlider = await pitchSliderRef.get()
+    let value = getSliderValue(pitchSlider) / 100
+    if (value < 0.5) {
+        value += 0.5
+    } else {
+        value = (value - 0.5) / 1.5 + 1
+    }
+    for (const control of audioControls) {
+        control.pitchMultiplier = value
+    }
+}
+
+async function updateModifiers() {
+    updateVolume()
+    updatePitch()
 }
 
 class AudioControl {
@@ -171,8 +203,10 @@ class AudioControl {
     readonly audioContext: AudioContext
     readonly audioBuffer: AudioBuffer
     readonly audioGain: GainNode
+    readonly category: string
     currentlyPlaying: boolean = false
     startTime: number
+    pitchMultiplier = 1
     private requestPlayAfterStop: boolean = false
 
     private audioSource: AudioBufferSourceNode | undefined
@@ -181,13 +215,15 @@ class AudioControl {
         buttonElement: HTMLButtonElement,
         audioContext: AudioContext,
         audioBuffer: AudioBuffer,
-        audioGain: GainNode
+        audioGain: GainNode,
+        category: string
     ) {
         this.buttonElement = buttonElement
         this.audioContext = audioContext
         this.audioBuffer = audioBuffer
         this.audioGain = audioGain
         this.startTime = audioContext.currentTime
+        this.category = category
     }
 
     play() {
@@ -200,6 +236,8 @@ class AudioControl {
         this.startTime = this.audioContext.currentTime
         const newAudioSource = this.audioContext.createBufferSource()
         this.audioSource = newAudioSource
+        this.audioSource.loop = loopMode
+        this.audioSource.detune.setValueAtTime(Math.log2(this.pitchMultiplier) * 1200, 0)
         this.audioSource.buffer = this.audioBuffer
         this.audioSource.connect(this.audioGain)
         this.audioSource.start(0)
@@ -221,15 +259,15 @@ class AudioControl {
     }
 
     getElapsedTime() {
-        if (!this.currentlyPlaying) {
+        if (!this.currentlyPlaying || this.audioSource === undefined) {
             return 0
         }
         const currentTime = this.audioContext.currentTime - this.startTime
-        return currentTime / this.audioBuffer.duration
+        return currentTime / this.audioBuffer.duration * this.pitchMultiplier
     }
 }
 
-async function createAudioButton(name: string, index: number) {
+async function createAudioButton(name: string, category: string, index: number) {
     const audioContext = new AudioContext()
     const audioBuffer = await audio.load(`../audio/${name}.mp3`, audioContext)
     const audioGain = audioContext.createGain()
@@ -243,10 +281,10 @@ async function createAudioButton(name: string, index: number) {
     buttonTitleElement.textContent = name
     buttonElement.appendChild(buttonTitleElement)
 
-    const audioControl = new AudioControl(buttonElement, audioContext, audioBuffer, audioGain)
+    const audioControl = new AudioControl(buttonElement, audioContext, audioBuffer, audioGain, category)
 
     buttonElement.onclick = async event => {
-        updateVolume()
+        updateModifiers()
         audioControl.play()
     }
 
@@ -257,24 +295,31 @@ async function createAudioButton(name: string, index: number) {
     return audioControl
 }
 
-async function addSound(controlContainer: HTMLElement, name: string, index: number) {
-    let audioControl = await createAudioButton(name, index)
+async function addSound(controlContainer: HTMLElement, name: string, category: string, index: number) {
+    let audioControl = await createAudioButton(name, category, index)
     controlContainer.appendChild(audioControl.buttonElement)
     audioControls.push(audioControl)
 }
 
 async function initAudioSlider() {
-    let audioSliderElement = await audioSliderRef.get()
-    audioSliderElement = document.getElementById('volume-slider') as HTMLInputElement
+    let volumeSlider = await volumeSliderRef.get()
+    let pitchSlider = await pitchSliderRef.get()
 
-    await updateVolume()
-    audioSliderElement.onchange = async event => {
-        await updateVolume()
+    await updateModifiers()
+    volumeSlider.onchange = async event => {
+        await updateModifiers()
+    }
+    pitchSlider.onchange = async event => {
+        await updateModifiers()
+    }
+    pitchSlider.oncontextmenu = async event => {
+        pitchSlider.value = '50'
+        await updateModifiers()
     }
 }
 
 async function playAll() {
-    updateVolume()
+    updateModifiers()
     for (const control of audioControls) {
         control.play()
     }
@@ -283,6 +328,14 @@ async function playAll() {
 async function stopAll() {
     for (const control of audioControls) {
         control.stop()
+    }
+}
+
+function playFriends() {
+    for (const control of audioControls) {
+        if (control.category === 'Friends') {
+            control.play()
+        }
     }
 }
 
@@ -324,7 +377,7 @@ async function start() {
         let i = 0
 
         for (const name of audioFiles[category]) {
-            addSound(container, name, i)
+            addSound(container, name, category, i)
             i++;
         }
     }
@@ -343,9 +396,18 @@ async function start() {
 
     const friendsButton = await playFriendsRef.get()
     friendsButton.onclick = () => {
-
+        playFriends()
     }
 
+    const loopModeButton = await loopModeRef.get()
+    loopModeButton.onclick = () => {
+        loopMode = !loopMode
+        if (loopMode) {
+            loopModeButton.setAttribute('active', 'active')
+        } else {
+            loopModeButton.removeAttribute('active')
+        }
+    }
 }
 
 start()
