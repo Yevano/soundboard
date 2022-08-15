a5ffaa93767c0800715b69806fccbe6cd895c7ed
James King <me@jamesking.email>
2022-08-15 00:38:11 -0400

    Add backend

---
+++ backend/src/main.ts
@@ -0,0 +1,94 @@
+import express from 'express'
+import { Dir, Dirent, existsSync, fstat } from 'node:fs'
+import { FileHandle, mkdir, open, opendir, readFile, writeFile } from 'node:fs/promises'
+import path from 'node:path'
+import multer from 'multer'
+
+import { FileMeta, StoreMeta } from './webapi-types'
+
+const app = express()
+const audioDirPath = 'store/audio'
+const storage = multer.diskStorage({ destination: audioDirPath })
+const storeAudio = multer({ storage })
+
+async function getStoreMeta() {
+    return JSON.parse((await readFile('store/metadata.json')).toString()) as StoreMeta
+}
+
+async function setStoreMeta(meta: StoreMeta) {
+    await writeFile('store/metadata.json', JSON.stringify(meta))
+}
+
+async function initStore() {
+    let storeDir: Dir | undefined
+    let audioDir: Dir | undefined
+    let metaFile: FileHandle | undefined
+
+    try {
+        storeDir = await opendir('store')
+    } catch (err) {
+        await mkdir('store')
+    } finally {
+        if (storeDir) {
+            await storeDir.close()
+        }
+    }
+
+    try {
+        audioDir = await opendir('store/audio')
+    } catch (err) {
+        await mkdir('store/audio')
+    } finally {
+        if (audioDir) {
+            await audioDir.close()
+        }
+    }
+
+    try {
+        metaFile = await open('store/metadata.json')
+    } catch (err) {
+        await writeFile('store/metadata.json', JSON.stringify({
+            fileMetas: [ ]
+        }))
+    } finally {
+        if (metaFile) {
+            metaFile.close()
+        }
+    }
+
+    app.listen(8081)
+}
+
+app.get('/list-audio', async (req, res) => {
+    const meta = await getStoreMeta()
+    res.append('Access-Control-Allow-Origin', '*')
+    res.send(JSON.stringify(meta.fileMetas))
+})
+
+app.get('/get-audio/:name', async (req, res) => {
+    const audioFileName = req.params.name
+    const absolutePath = path.resolve(`${audioDirPath}/${audioFileName}`)
+    console.log(`Send file: ${absolutePath}`)
+    res.append('Access-Control-Allow-Origin', '*')
+    res.sendFile(absolutePath)
+})
+
+app.post('/put-audio/:name', storeAudio.single('recording'), async (req, res) => {
+    const { name } = req.body as { name: string }
+    
+    console.log(req.file)
+    const storageFile = req.file
+
+    res.append('Access-Control-Allow-Origin', '*')
+
+    if (storageFile === undefined) {
+        res.json({ error: 'error' })
+    } else {
+        const meta = await getStoreMeta()
+        meta.fileMetas.push({ name, fileName: storageFile?.filename })
+        await setStoreMeta(meta)
+        res.json({ fileName: storageFile.filename })
+    }
+})
+
+initStore()
